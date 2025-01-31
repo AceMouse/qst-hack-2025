a09703cb7303e933e947b0e4ace86ad2657d5db5
AceMouse <humle000@gmail.com>
2025-01-31 07:21:52 +0100

    merge in changes

--- Case 3 - Qpurpose/Hackaton_Diffusion-QC.py
+++ Case 3 - Qpurpose/Hackaton_Diffusion-QC.py
@@ -1,10 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, transpile
-from qiskit.circuit.library import QFT, ModularAdderGate
+from qiskit.circuit.library import QFT, ModularAdderGate, RYGate, MCXGate
 from qiskit.visualization import plot_histogram
 from qiskit.quantum_info import Statevector
 from qiskit_aer import AerSimulator
+
+plot_style = {
+    "displaytext": {
+        # gate name : gate text to be displayed
+        "block_adv_dg": "O_A^\dagger",
+        "block_adv": "O_A",
+        "c_right": "controlled right shift",
+        "c_left": "controlled left shift",
+    },
+
+    "displaycolor": {
+
+        "block_adv_dg": [ 
+            "#18AA18", 
+            "#000000" 
+        ],
+
+        "block_adv":  [ 
+            "#ff1818", 
+            "#000000" 
+        ],
+    },
+}
 """import pyqsp
 from pyqsp import angle_sequence, response
 from pyqsp.poly import (polynomial_generators, PolyTaylorSeries)
@@ -59,6 +82,7 @@ def MCX_Shift_gate(n):
         qc.mcx([k for k in range(n-1-j)],n-1-j)         # Using multi controlled NOT gates 
     qc.x(0)
     return qc.to_gate()
+
 def Diffusion_prep(n,dt,nu):
     """
     n: number of spatial qubits. N = 2**n is the number of spatial grid points. 
@@ -81,7 +105,7 @@ def Diffusion_prep(n,dt,nu):
     qc.ch(0,1,ctrl_state = '0')          # Controlled Hadamard gate 
     return qc.to_gate()
 
-def Block_encoding(n,dt,nu,shift_implementation):
+def Diffusion_block_encoding(n,dt,nu,shift_implementation):
     """
     n : number of spatial qubits
     dt : timestep
@@ -112,7 +136,7 @@ def Block_encoding(n,dt,nu,shift_implementation):
     qc.append(adder.inverse(),qr2[:]+qr1[:])
     qc.append(S,qr1[:])
     qc.append(G.inverse(),qr1[:2])
-    return qc    
+    return qc.to_gate() 
     
 # The following function extracts QSVT angle sequences from the file 'QSP_angles.txt'
 # It contains the angle sequences for x^(5k) for 0<k<21
@@ -149,9 +173,216 @@ def compute_angle_seq(file = 'QSP_angles.txt'):
             angle_seqs.append(np.array(seq))   
     return angle_seqs
 
+def Advection_block_encoding(n, dt, c):
+#    n = 4
+#    dx = 0.1
+#    dt = 0.001
+#    c=1
+    
+    d = 4                               # The domain is fixed to be [0,4]
+    dx = d/2**n                
+    #qrl = QuantumRegister(n, name = "qrl")
+    #cl = ClassicalRegister(3, name="Class")
+    qra = QuantumRegister(3, name="Qa")
+    qrn = QuantumRegister(n, name="Qn")
+    beta = c*dt/(2*dx)
+    if beta>1:
+        print("Beta =c*dt/2dx needs to be <1")
+        exit(1)
+    theta0=2*np.acos(-beta)
+    theta1 = 2*np.acos(beta)
+    theta2=np.pi
+    theta3=0
+    
+    phi0=0
+    phi1=0
+    phi2=0
+    phi3=0
+    
+    #qc = QuantumCircuit(cl, qra, qrn, name = "block_adv")
+    qc = QuantumCircuit(qra, qrn, name = "block_adv")
+    qc.h(qra[1])
+    qc.h(qra[2])
+    
+    """
+    
+    qc.x(qra[1])
+    qc.x(qra[2])
+    ry0 = RYGate(theta0).control(2)
+    qc.append(ry0([1, 2, 0]))
+    qc.x(qra[1])
+    qc.x(qra[2])
+    
+    qc.x(gra[1])
+    ry1 = RYGate(theta1).control(2)
+    qc.append(ry0([1, 2, 0]))
+    qc.x(qra[1])
+    
+    qc.x(gra[2])
+    ry2 = RYGate(theta2).control(2)
+    qc.append(ry2([1, 2, 0]))
+    qc.x(qra[2])
+    """
+    
+    ry0 = RYGate(theta0).control(2, ctrl_state = '00')
+    ry1 = RYGate(theta1).control(2, ctrl_state = '10')
+    ry2 = RYGate(theta2).control(2, ctrl_state = '01')
+    
+    qc.append(ry0, [qra[1], qra[2], qra[0]])
+    qc.append(ry1, [qra[1], qra[2], qra[0]])
+    qc.append(ry2, [qra[1], qra[2], qra[0]])
+    
+    
+    
+    
+    #left shift
+    L = QuantumCircuit(qrn, name = "left")
+    
+    for k in range(0, n-1):
+        #L.append(MCXGate(num_ctrl_qubits = n-k-1), [qrl[k+1:n-1], qrl[k]])
+        gate = MCXGate(num_ctrl_qubits = n-k-1)
+        L.append(gate, list(range(k+1,n)) + [k])
+        #L.append(gate, [qrl[k+1:n-1], qrl[k]])
+        #L.append(gate, [qrl[k+1:n-1], qrl[k]])
+
+
+    L.x(n-1)
+    print(qc)
+    print(L)
+    
+    R = QuantumCircuit(qrn, name = "right")
+    for k in range(0, n-1):
+        gate = MCXGate(num_ctrl_qubits = n-k-1, ctrl_state = (n-k-1)*
+                       '0')
+        R.append(gate, list(range(k+1, n)) + [k])
+    
+    print(R)
+    
+    
+    LC = L.control().to_gate()
+    RC = R.control().to_gate()
+    
+    qc.append(LC, list(range(2, n+3)))
+    qc.append(RC, [1] +list(range(3, n+3)))
+    
+    qc.h(qra[1])
+    qc.h(qra[2])
+    #qc.barrier()
+    #qc.measure(qra[0], cl[0])
+    #qc.measure(qra[1], cl[1])
+    #qc.measure(qra[2], cl[2])
+#    qc.decompose(gates_to_decompose =["c_right", "c_left"]).draw(output="mpl",plot_barriers=False, fold=40, style=plot_style, scale=0.5).show()
+#    L.draw(output="mpl",plot_barriers=False, fold=40, style=plot_style, scale=0.5).show()
+#    R.draw(output="mpl",plot_barriers=False, fold=40, style=plot_style, scale=0.5).show()
+
+#    input()
+    
+    return qc
 # The following function implements QSVT on the block encoding provided by Block_encoding
 # The full circuit is then simulated with the aer-simulator using Gaussian initial conditions. 
 # A post-selection procedure picks out the successfull runs and arranges the results in a vector
+def Advection_QSVT(deg,n,dt,c,shots = 10**6,show_gate_count = False):
+    """
+    deg: number of time steps
+    n: number of spatial qubits 
+    dt: time step
+    nu: diffusion coefficient
+    shots: number of shots used in the aer-simulator
+    show_gate_count: True or False according to whether gate counts should be printed
+    
+    The function implements QSVT for the function x**deg on Block_encoding(n,dt,nu)
+    yielding a quantum circuit. The circuit is then measured in the computational basis. 
+    
+    The circuit is initalized with normalized Gaussian initial conditions over
+    [0,d=4] with N = 2**n uniformly distributed grid points.  
+    
+    The circuit is simulated using the aer-simulator with shots = shots. 
+    
+    A post-selection procedure picks out the successfull runs and arranges the results in
+    a vector z.
+
+    Returns x,z the spatial grid values and the simulated y values in z. 
+    """
+    sim = AerSimulator()
+    # Setting up the circuit 
+    qra = QuantumRegister(1)      # Ancilla register on 1 qubit used in QSVT 
+    qrab = QuantumRegister(3)      # This will store the ancillary qubits needed for the block encoding
+    qrn = QuantumRegister(n)      #This will store the system qubits made in the block encoding
+    
+    cra = ClassicalRegister(1)
+    crab = ClassicalRegister(3)
+    crn = ClassicalRegister(n)
+    
+    qc = QuantumCircuit(qra, qrab, qrn, cra, crab, crn)
+    
+    # Preparing the initial conditions 
+    N = 2**n 
+    d = 4                                             # spatial domain [0,d]
+    x = np.linspace(0,d,N,endpoint = False)
+    y = np.exp(-20*(x-d/3)**2)                        # Gaussian initial conditions 
+    y = y/np.linalg.norm(y)                           # normalized to be a unit vector
+    qc.prepare_state(Statevector(y),qrn)
+    U = Advection_block_encoding(n,dt,c)                       # Block encoding circuit 
+    Phi = extract_angle_seq()[int(deg/5)-1]           # Extracting the angle sequence  
+    
+    # Applying the QSVT circuit 
+    qc.h(qra[0])
+    s = 0
+    for k in range(len(Phi)-1,-1,-1):
+        if s == 0:
+            qc.append(U,qrab[:]+qrn[:])
+            s = 1
+        else:
+            qc.append(U.inverse(),qrab[:]+qrn[:])
+            s = 0
+        qc.mcx(qrn[:],qra[0],ctrl_state = n*'0')
+        qc.rz(2*Phi[k],qra[0])
+        qc.mcx(qrn[:],qra[0],ctrl_state = n*'0')
+    qc.h(qra[0])
+    
+    # Measurements
+    qc.barrier()
+    qc.measure(qra,cra)
+    qc.measure(qrab,crab)
+    qc.measure(qrn,crn)
+    
+    # Running the circuit     
+#    qc.draw(output="mpl",plot_barriers=False, fold=40, scale=0.5, style=plot_style).show()
+    qc_comp = transpile(qc,sim)
+    gate_1q = 0
+    gate_2q = 0
+    dict = qc_comp.count_ops()
+    for key in dict:
+        if key[0] == 'c':
+            gate_2q += dict[key]
+        elif key != 'measure':
+            gate_1q += dict[key]
+
+
+    if show_gate_count:
+        print("1 qubit gates:", gate_1q)
+        print("2 qubit gates:", gate_2q)
+        print("Total:", gate_1q + gate_2q )
+        print('Circuit depth after transpiling:', qc_comp.depth())
+        
+    # Postselection
+    res = sim.run(qc_comp,shots = shots).result()
+    counts = res.get_counts(0)
+    #plot_histogram(counts,legend=["counts"], color=['crimson','midnightblue'],
+    #            title="New Histogram").show()
+    select = (3+1)*'0'
+    total = 0                      # Tracks the number of successfull outcomes
+    z = np.zeros(N)                # The results are encoded in z 
+    for key in counts:
+        L = key.split()
+        if L[1]+L[2] == select:
+            z[int(L[0],2)] = np.sqrt(counts[key]/shots)    # By construction all amplitudes are positive real numbers
+            total += counts[key]                           # so this actually recovers them!
+    success_rate = total/shots
+    print('Success rate =', success_rate)
+    d=4
+    x = np.linspace(0,d,N,endpoint = False)
+    return x,z
 
 def Diffusion_QSVT(deg,n,dt,nu,shots = 10**6,show_gate_count = False):
     """
@@ -201,7 +432,7 @@ def Diffusion_QSVT(deg,n,dt,nu,shots = 10**6,show_gate_count = False):
         y = np.exp(-20*(x-d/3)**2)                        # Gaussian initial conditions 
         y = y/np.linalg.norm(y)                           # normalized to be a unit vector
         qc.prepare_state(Statevector(y),qr2)
-        U = Block_encoding(n,dt,nu,shift_implementation)                       # Block encoding circuit 
+        U = Diffusion_block_encoding(n,dt,nu,shift_implementation)                       # Block encoding circuit 
         Phi = extract_angle_seq()[int(deg/5)-1]           # Extracting the angle sequence  
         
         # Applying the QSVT circuit 
@@ -266,7 +497,7 @@ def Diffusion_QSVT(deg,n,dt,nu,shots = 10**6,show_gate_count = False):
         f.write(f"{deg}\t{n}\t{dt}\t{nu}\t{gates[min_idx][0]}\t{gates[min_idx][1]}\t{sum(gates[min_idx])}\t{depths[min_idx]}\t{success_rate}\n")
     return x,z
 
-def Euler_cl(deg,n,dt,nu):
+def Diffusion_euler_cl(deg,n,dt,nu):
     """
     deg: number of time steps
     n: N=2**n is the number of spatial grid points
@@ -294,19 +525,65 @@ def Euler_cl(deg,n,dt,nu):
     w = np.matmul(C,y)
     return x,y,w 
 
-def Compare_plots(deg = 10,n = 5,dt = 0.1,nu = 0.02,shots = 10**6, plot=False):
+def Advection_euler_cl(deg,n,dt,c):
+    """
+    deg: number of time steps
+    n: N=2**n is the number of spatial grid points
+    dt: time step 
+    c: speed coefficient 
+    
+    Returns x,y,w where x are the spatial grid points, 
+    y and z are the function values at x at time t=0 and t=deg*dt, respectively.
+    """    
+    N = 2**n
+    d = 4                     # Domain [0,d]
+    dx = d/N 
+    x = np.linspace(0,d,N,endpoint = False)
+    
+    b = c*dt/(2*dx)
+    if (b>=1): 
+        raise Exception(f"Advection error: beta over 1! beta = {b}")
+    B = -b*np.diag(np.ones(N-1),-1)+b*np.diag(np.ones(N-1),1)
+    B[0][-1] = b
+    B[-1][0] = -b
+    print(B)
+    
+    y = np.exp(-20*(x-d/3)**2)
+    y = y/np.linalg.norm(y)
+    
+    C = np.linalg.matrix_power(B,deg)
+    w = np.matmul(C,y)**2
+    w = w/np.linalg.norm(w)
+    return x,y,w+1  
+
+def Diffusion_compare_plots(deg = 10,n = 5,dt = 0.1,nu = 0.02,shots = 10**6, plot=False):
     # Plots the initial distribution and the results of the classical and quantum simulations at t = deg*dt  
-    x,y,w = Euler_cl(deg,n,dt,nu)  
+    x,y,w = Diffusion_euler_cl(deg,n,dt,nu)  
     x,z = Diffusion_QSVT(deg,n,dt,nu,shots = shots, show_gate_count = True)
     T = deg*dt 
     if (plot):
+        plt.figure(6)
         plt.plot(x,y,x,w,x,z)
         plt.legend(['Classical T=0','Classical T='+str(T),'Quantum T='+str(T)])
         plt.show()
 
+def Advection_compare_plots(deg = 10,n = 5,dt = 0.1,c = 3,shots = 10**6, plot=False):
+    # Plots the initial distribution and the results of the classical and quantum simulations at t = deg*dt  
+    #x,y,w = Advection_euler_cl(deg,n,dt,c)  
+    x,y = Advection_QSVT(0,n,dt,c,shots = shots, show_gate_count = True)
+    x,z = Advection_QSVT(deg,n,dt,c,shots = shots, show_gate_count = True)
+    z = z/np.linalg.norm(z)
+    T = deg*dt 
+    if (plot):
+        plt.figure(7)
+        plt.plot(x,y,x,z)
+        plt.legend(['Quantum T=0','Quantum T='+str(T)])
+        plt.show()
+
 Shift_implementations = [QFT_Shift_gate, MCX_Shift_gate]
-Compare_plots(deg = 10,n = 6,dt = 0.05,nu = 0.02, shots = 10**6, plot=True)
-for x in [[QFT_Shift_gate],[MCX_Shift_gate],[QFT_Shift_gate, MCX_Shift_gate]]:
+#Diffusion_compare_plots(deg = 10, n = 6, dt = 0.05, nu = 0.02, shots = 10**6, plot=True)
+Advection_compare_plots(deg = 1, n = 6, dt = 0.2, c = 0.2, shots = 10**6, plot=True)
+"""for x in [[QFT_Shift_gate],[MCX_Shift_gate],[QFT_Shift_gate, MCX_Shift_gate]]:
     Shift_implementations = x 
     with open(f"{'_'.join([x.__name__ for x in Shift_implementations])}.dat", "w") as f:
         f.write("deg\tn\tdt\tnu\tgates_q1\tgates_q2\tgates_total\tcircuit_depth\tsucces_rate\n")
@@ -320,3 +597,4 @@ for n in range(2,10):
         dt = ((dx**2)/(2*nu))/2
         print(f"dt: {dt}")
         Compare_plots(deg = 20,n = n,dt = dt,nu = nu, shots = 10**6)
+"""
